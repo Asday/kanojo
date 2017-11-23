33c03aa337907c7d1ece8eab0f865559fc91238b
asday <sara.and.zuka@gmail.com>
2017-11-23 19:30:13 +0000

    Added example state for the reader.

---
+++ reducers/reader.js
@@ -0,0 +1,13 @@
+const exampleState = {
+  URL: 'https://example.com/hero_aca/',
+  currentPage: 0,
+  pageDataLoading: false,
+  pages: [
+    {
+      imageURL: 'https://example.com/image.jpg',
+      imageData: '',  // b64 encoded image?
+      loading: false,
+    },
+    // ...
+  ],
+}
