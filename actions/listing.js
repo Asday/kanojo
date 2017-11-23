cadd36e9c2862c74427652d99329de11bce74805
asday <sara.and.zuka@gmail.com>
2017-11-23 13:24:01 +0000

    Fully implemented `reducers.listing`.

--- actions/listing.js
+++ actions/listing.js
@@ -1,9 +1,51 @@
-export const LISTINGS_LOADED = 'listings/LISTINGS_LOADED'
+export const REQ_LOAD_LISTINGS = 'listings/REQ_LOAD_LISTINGS'
+export const REQ_LOAD_LISTINGS_SUCCESS = 'listings/REQ_LOAD_LISTINGS_SUCCESS'
+export const REQ_LOAD_LISTINGS_FAILURE = 'listings/REQ_LOAD_LISTINGS_FAILURE'
 export const loadListings = () => (dispatch) => {
-  // Dispatch optimistic stuff, set off long running tasks, dispatch their results...
-
   dispatch({
-    type: LISTINGS_LOADED,
+    type: REQ_LOAD_LISTINGS,
     payload: {},
   })
+
+  fetch('https://example.com')
+    .then((data) => {
+      dispatch(loadListingsPage(data))
+    })
+    .catch((error) => {
+      dispatch({
+        type: REQ_LOAD_LISTINGS_FAILURE,
+        payload: {
+          error,
+        },
+      })
+    })
+}
+
+export const REQ_LOAD_LISTINGS_PAGE = 'listings/REQ_LOAD_LISTINGS_PAGE'
+export const REQ_LOAD_LISTINGS_PAGE_SUCCESS =
+  'listings/REQ_LOAD_LISTINGS_PAGE_SUCCESS'
+export const REQ_LOAD_LISTINGS_PAGE_FAILURE =
+  'listings/REQ_LOAD_LISTINGS_PAGE_FAILURE'
+export const loadListingsPage = (pageURL) => (dispatch) => {
+  dispatch({
+    type: REQ_LOAD_LISTINGS_PAGE,
+    payload: {},
+  })
+
+  fetch(pageURL)
+    .then((data) => {
+      // Parse `data` into [{title: '', coverURL: '', URL: ''}].
+      dispatch({
+        type: REQ_LOAD_LISTINGS_PAGE_SUCCESS,
+        payload: data,
+      })
+    })
+    .catch((error) => {
+      dispatch({
+        type: REQ_LOAD_LISTINGS_PAGE_FAILURE,
+        payload: {
+          error,
+        },
+      })
+    })
 }
--- reducers/listing.js
+++ reducers/listing.js
@@ -1,17 +1,72 @@
+import { unionBy } from 'lodash';
 import { combineReducers } from 'redux'
 
-import { LISTINGS_LOADED } from '../actions/listing'
+import {
+  REQ_LOAD_LISTINGS_SUCCESS,
+  REQ_LOAD_LISTINGS_FAILURE,
+  REQ_LOAD_LISTINGS_PAGE_SUCCESS,
+} from '../actions/listing'
 
 const initialIsLoading = true
-const isLoading  = (state = initialIsLoading, { type }) => {
+const isLoading = (state = initialIsLoading, { type }) => {
   switch (type) {
-    case LISTINGS_LOADED:
-      return false
+    case REQ_LOAD_LISTINGS_SUCCESS:
+
+    return false
   }
 
   return state
 }
 
+const initialIsAnyLoaded = false
+const isAnyLoaded = (state = initialIsAnyLoaded, { type }) => {
+  switch (type) {
+    case REQ_LOAD_LISTINGS_PAGE_SUCCESS:
+
+    return true
+  }
+
+  return state
+}
+
+const initialIsErrored = false
+const isErrored = (state = initialIsErrored, { type }) => {
+  switch (type) {
+    case REQ_LOAD_LISTINGS_FAILURE:
+
+    return true
+  }
+
+  return state
+}
+
+initialItems = []
+const items = (state = initialItems, { type, payload }) => {
+  switch (type) {
+    case REQ_LOAD_LISTINGS_PAGE_SUCCESS:
+
+    return unionBy(payload, state, 'URL')
+  }
+  return state
+}
+
 export const listing = combineReducers({
   isLoading,
+  isAnyLoaded,
+  isErrored,
+  items,
 })
+
+// {
+//   isLoading: false,
+//   isAnyLoaded: true,
+//   isErrored: false,
+//   items: [
+//     {
+//       title: 'Boku no Hero Academia',
+//       coverURL: 'https://example.com/image.jpg',
+//       URL: 'https://example.com/hero_aca/',
+//     },
+//     ...
+//   ],
+// }
