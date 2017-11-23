5b59feb5aef4134828bd381e29edae5a2072373c
asday <sara.and.zuka@gmail.com>
2017-11-23 21:26:19 +0000

    Added reader reducers.

--- reducers/index.js
+++ reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import { listing } from './listing'
+import { reader } from './reader'
 
 export const root = combineReducers({
   listing,
+  reader,
 })
--- reducers/reader.js
+++ reducers/reader.js
@@ -1,3 +1,109 @@
+import { map, unionBy } from 'lodash'
+import { combineReducers } from 'redux'
+
+import { READ } from '../actions/listing'
+import {
+  NAVIGATE_TO_NEXT_PAGE,
+  NAVIGATE_TO_PAGE,
+  NAVIGATE_TO_PREVIOUS_PAGE,
+  REQ_LOAD_PAGE,
+  REQ_LOAD_PAGE_DATA,
+  REQ_LOAD_PAGE_DATA_FAILURE,
+  REQ_LOAD_PAGE_DATA_SUCCESS,
+  REQ_LOAD_PAGE_FAILURE,
+  REQ_LOAD_PAGE_SUCCESS,
+} from '../actions/reader'
+
+const initialURL = null
+const URL = (state = initialURL, { type, payload }) => {
+  switch (type) {
+    case READ:
+
+    return payload.URL
+  }
+
+  return state
+}
+
+const initialCurrentPage = 0
+const currentPage = (state = initialCurrentPage, { type, payload }) => {
+  switch (type) {
+    case NAVIGATE_TO_NEXT_PAGE:
+
+    return state + 1
+
+    case NAVIGATE_TO_PREVIOUS_PAGE:
+
+    return state - 1
+
+    case NAVIGATE_TO_PAGE:
+
+    return payload.index
+  }
+
+  return state
+}
+
+const initialIsPageDataLoading = false
+const isPageDataLoading = (
+    state = initialIsPageDataLoading,
+    { type, payload }) => {
+  switch (type) {
+    case REQ_LOAD_PAGE_DATA:
+
+    return true
+
+    case REQ_LOAD_PAGE_DATA_SUCCESS:
+    case REQ_LOAD_PAGE_DATA_FAILURE:
+
+    return false
+  }
+
+  return state
+}
+
+const initialPages = []
+const pages = (state = initialPages, { type, payload }) => {
+  switch (type) {
+    case REQ_LOAD_PAGE_DATA_SUCCESS:
+
+    return unionBy([payload], state, 'index')
+
+    case REQ_LOAD_PAGE:
+
+    return map(state, (value) => (
+      value.index === payload.index
+        ? { ...value, loading: true }
+        : value
+    ))
+
+    case REQ_LOAD_PAGE_SUCCESS:
+
+    return map(state, (value) => (
+      value.index === payload.index
+        ? { ...value, loading: false, imageData: payload.imageData }
+        : value
+    ))
+
+    case REQ_LOAD_PAGE_FAILURE:
+
+    return map(state, (value) => (
+      value.index === payload.index
+        ? { ...value, loading: false }
+        : value
+    ))
+  }
+
+  return state
+}
+
+export const reader = combineReducers({
+  URL,
+  currentPage,
+  isPageDataLoading,
+  pages,
+})
+
 const exampleState = {
   URL: 'https://example.com/hero_aca/',
   currentPage: 0,
