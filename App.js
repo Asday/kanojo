f1fb34c7f676033e78261403fb4c2094aa42d057
asday <sara.and.zuka@gmail.com>
2017-11-23 00:50:21 +0000

    Provided state to the app.

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import Main from './containers/Main'
+import { root } from './reducers'
+
+const store = createStore(root)
 
 export default class App extends React.Component {
   render() {
     return (
-      <Main />
+      <Provider store={ store }>
+        <Main />
+      </Provider>
     )
   }
 }
