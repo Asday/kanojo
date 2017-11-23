5123aeaabd84ce260df6b44869b37163bcbb51f7
asday <sara.and.zuka@gmail.com>
2017-11-23 19:48:09 +0000

    Added reader actions.

---
+++ actions/reader.js
@@ -0,0 +1,48 @@
+export const NAVIGATE_TO_NEXT_PAGE = 'reader/NAGIVATE_TO_NEXT_PAGE'
+export const navigateToNextPage = () => ({
+  type: NAVIGATE_TO_NEXT_PAGE,
+  payload: {},
+})
+
+export const NAVIGATE_TO_PREVIOUS_PAGE = 'reader/NAVIGATE_TO_PREVIOUS_PAGE'
+export const navigateToPreviousPage = () => ({
+  type: NAVIGATE_TO_PREVIOUS_PAGE,
+  payload: {},
+})
+
+export const NAVIGATE_TO_PAGE = 'reader/NAVIGATE_TO_PAGE'
+export const navigateToPage = (pageNumber) => ({
+  type: NAVIGATE_TO_PAGE,
+  payload: {
+    pageNumber,
+  },
+})
+
+export const REQ_LOAD_PAGE_DATA = 'reader/REQ_LOAD_PAGE_DATA'
+export const REQ_LOAD_PAGE_DATA_SUCCESS = 'reader/REQ_LOAD_PAGE_DATA_SUCCESS'
+export const REQ_LOAD_PAGE_DATA_FAILURE = 'reader/REQ_LOAD_PAGE_DATA_FAILURE'
+export const loadPageData = (URL) => (dispatch) => {
+  dispatch({
+    type: REQ_LOAD_PAGE_DATA,
+    payload: {},
+  })
+
+  // TODO:  Fetch the page listing page.
+  // TODO:  Fetch each of the page pages.  (Good lord).
+  // TODO:  Parse the image URL from the page page.
+}
+
+export const REQ_LOAD_PAGE = 'reader/REQ_LOAD_PAGE'
+export const REQ_LOAD_PAGE_SUCCESS = 'reader/REQ_LOAD_PAGE_SUCCESS'
+export const REQ_LOAD_PAGE_FAILURE = 'reader/REQ_LOAD_PAGE_FAILURE'
+export const loadPage = ({ index, imageURL }) => (dispatch) => {
+  dispatch({
+    type: REQ_LOAD_PAGE,
+    payload: {
+      index,
+    },
+  })
+
+  // TODO:  Call some function to fetch the image and give it to us as
+  // b64.
+}
