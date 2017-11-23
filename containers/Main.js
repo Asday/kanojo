6134f4dc6585912f91faf9eafd652eb62e2098be
asday <sara.and.zuka@gmail.com>
2017-11-23 00:50:50 +0000

    Demonstrated reading from state in the app.

--- containers/Main.js
+++ containers/Main.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
 import DemoText from '../components/DemoText'
+import DemoText2 from '../components/DemoText2'
+import { isLoadingSelector } from '../selectors/listing'
 
-export default class Main extends React.Component {
+const select = (state) => ({
+  isLoading: isLoadingSelector(state),
+})
+
+export class Main extends React.Component {
   render = () => {
+    const {
+      isLoading,
+    } = this.props
+
     return (
-      <DemoText />
+      isLoading ? <DemoText /> : <DemoText2 />
     )
   }
 }
+
+export default connect(select)(Main)
