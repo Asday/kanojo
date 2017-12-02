6e08c83ec109506ab4804c6d5056aa59b027ff52
asday <sara.and.zuka@gmail.com>
2017-12-02 23:38:58 +0000

    Implemented scrolling through listings.

--- components/Listing.js
+++ components/Listing.js
@@ -1,6 +1,6 @@
 import { map } from 'lodash'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, FlatList } from 'react-native'
 
 import ItemPanel from './ItemPanel'
 
@@ -11,19 +11,19 @@ export default class Listing extends React.Component {
     } = this.props
 
     return (
-      <View style={ styles.view }>
-        {
-          map(items, (item, key) => (
-            <ItemPanel item={ item } key={ key } />
-          ))
-        }
-      </View>
+      <FlatList
+        style={ styles.flatList }
+        data={ items }
+        keyExtractor={ (item) => item.URL }
+        numColumns={ 4 }
+        renderItem={ ({ item }) => <ItemPanel item={ item } /> }
+      />
     )
   }
 }
 
 const styles = StyleSheet.create({
-  view: {
+  flatList: {
     flex: 1,
   },
 })
